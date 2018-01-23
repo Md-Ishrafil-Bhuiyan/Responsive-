5c445902e228ef333d7c12a4a554b858d518c298
unknown <you@example.com>
2018-01-23 09:39:47 +0600

    responsive site

---
+++ js/carosel.js
@@ -0,0 +1,18 @@
+.carousel - control.left,
+    .carousel - control.right {
+        opacity: 1;
+        filter: alpha(opacity = 100);
+        background - image: none;
+        background - repeat: no - repeat;
+        text - shadow: none;
+        margin - right: 95 px;
+    }
+    .carousel - indicators.active {
+        background - color: #ff5900;
+    }
+    .carousel - control {
+        color: #e2e2e2;
+    }
+    .carousel - control {
+        width: 90;
+    }
